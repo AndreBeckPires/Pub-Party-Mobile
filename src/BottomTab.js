91fb827c42a4163d35671f1238699adfa716126b
Arthur <arthurmaiav@gmail.com>
2020-07-02 21:15:27 -0300

    Adicionada secao Scan

--- src/BottomTab.js
+++ src/BottomTab.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 
 import Home from './pages/Home';
 import Ranking from './pages/Ranking';
+import Scan from './pages/Scan'
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
@@ -10,6 +12,7 @@ export default function MyTabs() {
     return (
       <Tab.Navigator>
         <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Scan" component={Scan} />
         <Tab.Screen name="Ranking" component={Ranking} />
       </Tab.Navigator>
     );
