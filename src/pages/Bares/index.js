8346b0a0bcd399e4d8417f9032603aa544447235
André Pires <andrebeckpires@gmail.com>
2020-07-04 17:55:37 -0300

    Modal da party

--- src/pages/Bares/index.js
+++ src/pages/Bares/index.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, Constants, Image } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native'
+import { Text, View, Constants, Image, TouchableOpacity } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Modal from 'react-native-modal';
 
 import styles from './styles';
 
@@ -8,48 +11,59 @@ import barAmbev from '../../assets/barambev.png';
 import barLgbt from '../../assets/barlgbt.png';
 import barExpensive from '../../assets/barexpensive.png';
 
-
+Icon.loadFont();
 
 export default function Bares() {
 
 
+  function navigateBack() {
+    navigation.goBack();
+  }
   const navigation = useNavigation();
   const route = useRoute();
   const [state1, setState1] = useState([false]);
   const [state2, setState2] = useState([false]);
   const [state3, setState3] = useState([false]);
-  
+  const [isModalVisible, setModalVisible] = useState(false);
+
   const { tag } = route.params;
 
-  useEffect(()=>{
-    if (tag === 1) {
-      setState1({ show: true });
-  } 
-  if (tag === 2) {
-    
-      setState2({ show: true });
-  }
-  if (tag === 3) {    
-      setState3({ show: true });   
+  function showModal() {
+    setModalVisible(!isModalVisible);
   }
 
-}, []);
-  
+  useEffect(() => {
+    if (tag === 1) {
+      setState1({ show: true });
+    }
+    if (tag === 2) {
+
+      setState2({ show: true });
+    }
+    if (tag === 3) {
+      setState3({ show: true });
+    }
+
+  }, []);
+
   return (
     <View>
+
 
 
       {
         state1.show ? (
           <View>
-          <Image source={barLgbt}/>
+            <Image source={barLgbt} />
+            <Text> Dados do bar LGBT</Text>
           </View>
         ) : null
       }
-       {
+      {
         state2.show ? (
           <View>
-          <Image source={barAmbev}/>
+            <Image source={barAmbev} />
+            <Text> Dados do bar Ambev</Text>
           </View>
         ) : null
       }
@@ -57,12 +71,40 @@ export default function Bares() {
       {
         state3.show ? (
           <View>
-          <Image source={barExpensive}/>
+            <Image source={barExpensive} />
+            <Text> Dados do bar Expensive</Text>
           </View>
-        ) :  null
+        ) : null
       }
+      <TouchableOpacity
+        onPress={() => navigateBack()}>
 
+        <Icon name="arrow-back" color="black" size={26} />
+        <Text>Voltar</Text>
+      </TouchableOpacity>
 
+      <TouchableOpacity
+        onPress={() => showModal()}>
+        <MaterialCommunityIcons name="account-group" color={'black'} size={26} />
+        <Text>Formar grupo</Text>
+      </TouchableOpacity>
+    
+
+     <View style={{ flex: 1 }}>
+    
+
+        <Modal isVisible={isModalVisible}>
+          <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <Text>Forme sua party</Text>
+            
+            <TouchableOpacity
+        onPress={() => showModal()}>
+       <Icon name="arrow-back" color="black" size={26} />
+        <Text>Voltar</Text>
+      </TouchableOpacity>
+          </View>
+        </Modal>
+      </View>
 
 
     </View>
