49d53de05318a970dc5145336546a409a328645f
André Pires <andrebeckpires@gmail.com>
2020-07-04 16:47:41 -0300

    testes dos bares

--- src/pages/Bares/index.js
+++ src/pages/Bares/index.js
@@ -1,22 +1,69 @@
-import * as React from 'react';
-import { Text, View, Constants } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, Constants, Image } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native'
 
 import styles from './styles';
 
+import barAmbev from '../../assets/barambev.png';
+import barLgbt from '../../assets/barlgbt.png';
+import barExpensive from '../../assets/barexpensive.png';
+
+
+
 export default function Bares() {
+
+
   const navigation = useNavigation();
   const route = useRoute();
-  const [state, setState] = useState([true]);
-  const {tag} = route.params;
-  return (
-
-    <View>
-      <Text >1</Text>    
-      <Text >2</Text>  
-      <Text >3</Text>  
+  const [state1, setState1] = useState([false]);
+  const [state2, setState2] = useState([false]);
+  const [state3, setState3] = useState([false]);
   
-        
+  const { tag } = route.params;
+
+  useEffect(()=>{
+    if (tag === 1) {
+      setState1({ show: true });
+  } 
+  if (tag === 2) {
+    
+      setState2({ show: true });
+  }
+  if (tag === 3) {    
+      setState3({ show: true });   
+  }
+
+}, []);
+  
+  return (
+    <View>
+
+
+      {
+        state1.show ? (
+          <View>
+          <Image source={barLgbt}/>
+          </View>
+        ) : null
+      }
+       {
+        state2.show ? (
+          <View>
+          <Image source={barAmbev}/>
+          </View>
+        ) : null
+      }
+
+      {
+        state3.show ? (
+          <View>
+          <Image source={barExpensive}/>
+          </View>
+        ) :  null
+      }
+
+
+
 
     </View>
   );
